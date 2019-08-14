835ada891732d621b96f1ddd0fe642127470e90c
Dancer-14863 <g14863.malika@gmail.com>
2019-08-14 16:01:01 +0530

    Added feature to display probe title on modal

--- assign_probe.php
+++ assign_probe.php
@@ -3,7 +3,6 @@
     Filename: upload_probe.php
     Author: Malika Liyanage
 */
-echo "<p>:D</p>";
 session_start();
 // If the user is not logged in redirect to the login page...
 if (!isset($_SESSION['logged_in'])) {
@@ -15,10 +14,9 @@ if (!isset($_SESSION['logged_in'])) {
 	header('Location: login_auth_one.php');
 	exit();
 }
-
 // Current settings to connect to the user account database
 require('user_db_connection.php');
-$dbname = 'GMI_US';
+$dbname = $_POST['project_name'];
 // Setting up the DSN
 $dsn = 'mysql:host='.$host.';dbname='.$dbname;
 
@@ -42,6 +40,8 @@ catch(PDOException $e){
 $sql = 'SELECT probe_queue_id FROM probe_queue WHERE account_id = :account_id';
 $stmt = $pdo->prepare($sql);
 $stmt->execute(['account_id'=>$_SESSION['id']]);
+$probe_info = $stmt->fetch(PDO::FETCH_OBJ);
+$last_id = $probe_info->probe_queue_id;
 $row_count = $stmt->rowCount(PDO::FETCH_OBJ);
 
 if ($row_count == 0) {
@@ -55,4 +55,10 @@ if ($row_count == 0) {
     $stmt = $pdo->prepare($sql);
     $stmt->execute(['account_id'=>$_SESSION['id'],'probe_queue_id'=>$last_id, 'probe_being_handled'=>1]);
 }
+$sql = 'SELECT brand.brand_name, client_category.client_category_name, probe.probe_id FROM  probe_queue INNER JOIN probe ON probe_queue.probe_key_id = probe.probe_key_id INNER JOIN brand ON probe.brand_id = brand.brand_id INNER JOIN client_category ON probe.client_category_id  = client_category.client_category_id WHERE probe_queue_id = :probe_queue_id';
+$stmt = $pdo->prepare($sql);
+$stmt->execute(['probe_queue_id'=>$last_id]);
+$probe_info = $stmt->fetch(PDO::FETCH_OBJ);
+$return_arr[] = array("brand_name" => $probe_info->brand_name, "client_category_name" => $probe_info->client_category_name , "probe_id"=>$probe_info->probe_id);
+echo json_encode($return_arr);
 ?>
--- scripts/validate_probe_hunt.js
+++ scripts/validate_probe_hunt.js
@@ -12,6 +12,38 @@ function validate_project_name () {
     }
 }
 
+function get_probe_info() {
+    var project_name_element = document.getElementById('project_name');
+    var project_name = project_name_element.options[project_name_element.selectedIndex].value;
+    var formData = new FormData();
+    formData.append('project_name', project_name);
+    jQuery.ajax({
+        url: 'assign_probe.php',
+        type: 'POST',
+        data: formData,
+        dataType: 'JSON',
+        success: function (data) {
+            var title_string = project_name;
+            if (data[0].brand_name != null) {
+                title_string += ' ' + data[0].brand_name;
+            }            
+            if (data[0].client_category_name != null) {
+                title_string += ' ' + data[0].client_category_name;
+            }
+            if (data[0].probe_id != null) {
+                title_string += ' ' + data[0].probe_id;
+            }
+            jQuery('#add_probe_title').html(title_string);
+        },
+        error: function (data) {
+            alert("Error assigning probe. Please refresh");
+        },
+        cache: false,
+        contentType: false,
+        processData: false
+    });
+}
+
 
 jQuery(document).ready(function () {
     jQuery('#project_name').select2({
