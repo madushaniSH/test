93e979f16fb6f1dea42c628242185077380e2ef7
Dancer-14863 <g14863.malika@gmail.com>
2019-08-26 19:08:54 +0530

    Started export feature

--- export_projects.php
+++ export_projects.php
@@ -10,7 +10,7 @@ if (!isset($_SESSION['logged_in'])) {
 	header('Location: login_auth_one.php');
 	exit();
 } else {
-    if(!($_SESSION['role'] === 'Admin' || $_SESSION['role'] === 'Supervisor' || $_SESSION['role'] === 'SRT' || $_SESSION['role'] === 'SRT Analyst')){
+    if(!($_SESSION['role'] === 'Admin' || $_SESSION['role'] === 'Supervisor')){
         header('Location: index.php');
 	    exit();
     }
@@ -32,8 +32,14 @@ if(isset($_SESSION['out'])){
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"
+        integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
     <link rel="stylesheet" type="text/css" href="styles/main.css" />
     <script src="scripts/transition.js"></script>
+    <script src="scripts/export_details.js"></script>
     <!-- Prerenders font awesome-->
     <script type="text/javascript"> (function() { var css = document.createElement('link'); css.href = 'https://use.fontawesome.com/releases/v5.10.0/css/all.css'; css.rel = 'stylesheet'; css.type = 'text/css'; document.getElementsByTagName('head')[0].appendChild(css); })(); </script>
     <title>Product Hunt</title>
@@ -55,48 +61,5 @@ if(isset($_SESSION['out'])){
         </ul>
     </div>
 </nav>
-<?php
-if ($_SESSION['role'] === 'Admin' || $_SESSION['role'] === 'Supervisor'){
-    echo"
-    <a href=\"new_project.php\" class=\"btn btn-lg dashboard-btn\">
-    <span>
-        <i class=\"fas fa-meteor fa-2x\"></i>   
-    </span>
-    Create New Project
-</a>
-
-<a href=\"upload_probe.php\" class=\"btn btn-lg dashboard-btn\">
-    <span>
-        <i class=\"fas fa-rocket fa-2x\"></i>
-    </span>
-    Upload Probes
-</a>
-<a href=\"export_projects.php\" class=\"btn btn-lg dashboard-btn\">
-    <span>
-        <i class=\"fas fa-file-export fa-2x\"></i>
-    </span>
-    Export Project
-</a>"
-;
-
-}
-if($_SESSION['role'] === 'Admin' || $_SESSION['role'] === 'Supervisor' || $_SESSION['role'] === 'SRT'){
-    echo"
-<a href=\"probe_hunt.php\" class=\"btn btn-lg dashboard-btn\">
-    <span>
-        <i class=\"fas fa-th-list fa-2x\"></i>
-    </span>
-    Probe Hunt
-</a>";
-}
-if($_SESSION['role'] === 'Admin' || $_SESSION['role'] === 'Supervisor' || $_SESSION['role'] === 'SRT Analyst'){
-    echo"
-<a href=\"probe_qa.php\" class=\"btn btn-lg dashboard-btn\">
-    <span>
-        <i class=\"fas fa-search-plus fa-2x\"></i>
-    </span>
-    Quality Assurance
-</a>";
-}
-?>
+<button onclick="fetch_details()">Generate</button>
 </body>
