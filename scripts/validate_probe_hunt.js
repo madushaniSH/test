57c0a58b5fff22bea93d13293b834beaea105383
Dancer-14863 <g14863.malika@gmail.com>
2019-09-25 21:41:24 +0530

    Minor tweak

--- scripts/validate_probe_hunt.js
+++ scripts/validate_probe_hunt.js
@@ -645,7 +645,7 @@ function add_probe_product() {
         is_valid_form = false;
     } else {
         product_name_error.innerHTML = '';
-        var patt = /^[a-zA-Z 0-9\-\'\!\%\&\(\)\.\_\/\+\,]*$/;
+        var patt = /^[a-zA-Z 0-9\-\'\!\%\&\(\)\.\_\/\+\,\#\\\:\=\$\;]*$/;
         if (!patt.test(product_name)) {
             is_valid_form = false;
             product_name_error.innerHTML = 'Non English Product Name Entered';
@@ -668,7 +668,7 @@ function add_probe_product() {
         is_valid_form = false;
     } else {
         alt_design_name_error.innerHTML = '';
-        var patt = /^[a-zA-Z 0-9\-\'\!\%\&\(\)\.\_\/\+\,]*$/;
+        var patt = /^[a-zA-Z 0-9\-\'\!\%\&\(\)\.\_\/\+\,\#\\\:\=\$\;]*$/;
         if (!patt.test(alt_design_name)) {
             is_valid_form = false;
             alt_design_name_error.innerHTML = 'Non English Product Name Entered';
