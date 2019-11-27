017fc17959c9b7170ac3e1f1b572c1b9e393e7fd
root <g14863.malika@gmail.com>
2019-11-27 17:38:14 +0530

    Tweaked access permission for ticket manager

--- ticket_handler/ticket.php
+++ ticket_handler/ticket.php
@@ -9,7 +9,7 @@ if (!isset($_SESSION['logged_in'])) {
     header('Location: ../login_auth_one.php');
     exit();
 } else {
-    if (!($_SESSION['role'] === 'Admin' || $_SESSION['role'] === 'Supervisor' || $_SESSION['role'] === 'ODA Supervisor')) {
+    if (!($_SESSION['role'] === 'Admin' || $_SESSION['role'] === 'Supervisor')) {
         header('Location: ../index.php');
         exit();
     }
