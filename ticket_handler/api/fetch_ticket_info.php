a47d7367ddd68357899652cfc2c46148a0700526
root <g14863.malika@gmail.com>
2019-11-29 13:42:26 +0530

    Refactored fetch_ticket_info.php

--- ticket_handler/api/fetch_ticket_info.php
+++ ticket_handler/api/fetch_ticket_info.php
@@ -25,6 +25,89 @@ try {
     echo "<p>Connection to database failed<br>Reason: " . $e->getMessage() . '</p>';
     exit();
 }
+
+function fetchTicketFacingCount ($pdo, $ticket_id, $product_type) {
+    $sql = 'SELECT SUM(prod.product_facing_count)
+        FROM products prod
+            LEFT OUTER JOIN probe_product_info ppi 
+                ON prod.product_id = ppi.probe_product_info_product_id
+            LEFT OUTER JOIN probe p 
+                ON ppi.probe_product_info_key_id = p.probe_key_id
+            LEFT OUTER JOIN radar_sources rs 
+                ON prod.product_id = rs.radar_product_id
+            LEFT OUTER JOIN radar_hunt rh 
+                ON rs.radar_hunt_id = rh.radar_hunt_id
+            LEFT OUTER JOIN ref_product_info rpi 
+                ON prod.product_id = rpi.product_id
+            LEFT OUTER JOIN reference_info ri 
+                ON rpi.reference_info_id = ri.reference_info_id
+            LEFT OUTER JOIN project_tickets pt 
+                ON 
+                    p.probe_ticket_id = pt.project_ticket_system_id
+                OR 
+                    rh.radar_ticket_id = pt.project_ticket_system_id
+                OR
+                    ri.reference_ticket_id = pt.project_ticket_system_id
+            WHERE
+                (p.probe_ticket_id = :id OR rh.radar_ticket_id = :id OR ri.reference_ticket_id = :id)
+              AND
+                (prod.product_qa_status = "approved" OR prod.product_qa_status = "rejected" or prod.product_qa_status = "active")
+              AND
+                (prod.product_type = :product_type)';
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([
+        'id' => $ticket_id,
+        'product_type' => $product_type
+    ]);
+    $count = $stmt->fetchColumn();
+    $count == NULL ? $return_count = 0 : $return_count = $count;
+    return $return_count;
+}
+
+function fetchTicketProductCount ($pdo, $ticket_id, $product_type) {
+    if ($product_type !== "facing") {
+        $sql = 'SELECT COUNT(*)
+        FROM products prod
+            LEFT OUTER JOIN probe_product_info ppi 
+                ON prod.product_id = ppi.probe_product_info_product_id
+            LEFT OUTER JOIN probe p 
+                ON ppi.probe_product_info_key_id = p.probe_key_id
+            LEFT OUTER JOIN radar_sources rs 
+                ON prod.product_id = rs.radar_product_id
+            LEFT OUTER JOIN radar_hunt rh 
+                ON rs.radar_hunt_id = rh.radar_hunt_id
+            LEFT OUTER JOIN ref_product_info rpi 
+                ON prod.product_id = rpi.product_id
+            LEFT OUTER JOIN reference_info ri 
+                ON rpi.reference_info_id = ri.reference_info_id
+            LEFT OUTER JOIN project_tickets pt 
+                ON 
+                    p.probe_ticket_id = pt.project_ticket_system_id
+                OR 
+                    rh.radar_ticket_id = pt.project_ticket_system_id
+                OR
+                    ri.reference_ticket_id = pt.project_ticket_system_id
+            WHERE
+                (p.probe_ticket_id = :id OR rh.radar_ticket_id = :id OR ri.reference_ticket_id = :id)
+              AND
+                (prod.product_qa_status = "approved" OR prod.product_qa_status = "rejected" or prod.product_qa_status = "active")
+              AND
+                (prod.product_type = :product_type)';
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([
+            'id' => $ticket_id,
+            'product_type' => $product_type
+        ]);
+        $count = $stmt->fetchColumn();
+        $count == NULL ? $return_count = 0 : $return_count = $count;
+    } else {
+        $return_count = fetchTicketFacingCount($pdo, $ticket_id, $product_type);
+    }
+    return $return_count;
+}
+
+
+
 try {
     $sql = '
 SELECT
@@ -40,19 +123,7 @@ SELECT
     pt.ticket_last_mod_date,
     pt.ticket_escalate,
     pt.ticket_escalate_date,
-    pt.ticket_completion_date,
-    SUM(
-        CASE WHEN prod.product_type = \'brand\' THEN 1 ELSE 0 END
-    ) AS brand_count,
-    SUM(
-        CASE WHEN prod.product_type = \'sku\' THEN 1 ELSE 0 END
-    ) AS sku_count,
-    SUM(
-        CASE WHEN prod.product_type = \'dvc\' THEN 1 ELSE 0 END
-    ) AS dvc_count,
-    SUM(
-        CASE WHEN prod.product_type = \'facing\' THEN 1 ELSE 0 END
-    ) AS facing_count
+    pt.ticket_completion_date
 FROM
     project_tickets pt
 INNER JOIN
@@ -63,52 +134,25 @@ LEFT OUTER JOIN
     user_db.accounts b
 ON
     b.account_id = pt.ticket_last_mod_account_id
-LEFT OUTER JOIN
-    probe p
-ON
-    p.probe_ticket_id = pt.project_ticket_system_id
-LEFT OUTER JOIN
-    probe_product_info ppi
-ON
-    ppi.probe_product_info_key_id = p.probe_key_id
-LEFT OUTER JOIN
-    radar_hunt rh
-ON
-    rh.radar_ticket_id = pt.project_ticket_system_id
-LEFT OUTER JOIN
-    radar_sources rs
-ON
-    rs.radar_hunt_id = rh.radar_hunt_id
-LEFT OUTER JOIN
-    reference_info ri
-ON
-    ri.reference_ticket_id = pt.project_ticket_system_id
-LEFT OUTER JOIN
-    ref_product_info rpi
-ON
-    rpi.reference_info_id = ri.reference_info_id
-LEFT OUTER JOIN
-    products prod
-ON
-    prod.product_id = ppi.probe_product_info_product_id OR prod.product_id = rs.radar_product_id OR prod.product_id = rpi.product_id
-WHERE
-    (
-        (
-            prod.product_qa_status = "approved" OR(
-                prod.product_qa_status = "rejected" OR prod.product_qa_status = "active"
-            )
-        ) OR 1
-    ) AND(
-        DATE(pt.ticket_creation_time) >= :start_date AND DATE(pt.ticket_creation_time) <= :end_date
-    )
+WHERE 
+      DATE(pt.ticket_creation_time) >= :start_date AND DATE(pt.ticket_creation_time) <= :end_date
 GROUP BY
     1';
     $stmt = $pdo->prepare($sql);
     $stmt->execute([
-        'start_date' => $_POST['start_date'],
-        'end_date' => $_POST['end_date']
-    ]);
+            'start_date' => $_POST['start_date'],
+            'end_date' => $_POST['end_date']
+      ]);
     $ticket_info = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    for ($i = 0; $i < count($ticket_info); $i++){
+        $id = $ticket_info[$i]["project_ticket_system_id"];
+        $ticket_info[$i]["sku_count"] = fetchTicketProductCount($pdo, $id, "sku");
+        $ticket_info[$i]["brand_count"] = fetchTicketProductCount($pdo, $id, "brand");
+        $ticket_info[$i]["dvc_count"] = fetchTicketProductCount($pdo, $id, "dvc");
+        $ticket_info[$i]["facing_count"] = fetchTicketProductCount($pdo, $id, "facing");
+    }
+
 
 } catch (PDOException $e) {
     $ticket_info = $e->getMessage();
