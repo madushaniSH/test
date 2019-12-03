c81dc6b07e961bf56a9668a55a650251f8d00555
Dancer-14863 <g14863.malika@gmail.com>
2019-12-03 21:23:24 +0530

    Script tweak

---
+++ scripts/snow.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    const d = new Date();
+    const n = d.getMonth();
+    if (n === 11) {
+        //Start the snow default options you can also make it snow in certain elements, etc.
+        $(document).snowfall();
+        $(document).snowfall({image: "scripts/flake.png", minSize: 10, maxSize: 15, maxSpeed: 2, flakeCount: 10});
+        //$(document).snowfall('clear');
+    }
+});
