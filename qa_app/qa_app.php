e044b54dd493de063d766ac0b401bffbdaea0161
Dancer-14863 <g14863.malika@gmail.com>
2019-12-03 17:57:09 +0530

    Initial commit for QA App revamp

--- qa_app/qa_app.php
+++ qa_app/qa_app.php
@@ -64,6 +64,99 @@ try {
                     :label="'Dark Mode'"
             ></v-switch>
         </v-app-bar>
+        <v-content>
+            <v-row
+                    :align="'end'"
+                    :justify="'start'"
+                    id="filters"
+            >
+                <v-col
+                        cols="12"
+                        md="3"
+                >
+                    <v-autocomplete
+                            v-model="selectedProject"
+                            label="Select"
+                            chips
+                            hint="Select a Project"
+                            persistent-hint
+                            :items="projectArray"
+                            item-text="name"
+                            item-value="name"
+                    >
+                    </v-autocomplete>
+                </v-col>
+                <v-col
+                        cols="6"
+                        md="3"
+                >
+                    <v-autocomplete
+                            v-model="selectedTickets"
+                            label="Select"
+                            chips
+                            :items="ticketArray"
+                            item-text="ticket_id"
+                            item-value="ticket_id"
+                            multiple
+                    >
+                        <template v-slot:selection="{ item, index }">
+                            <v-chip v-if="index === 0">
+                                <span>{{ item.ticket_id }}</span>
+                            </v-chip>
+                            <span
+                                    v-if="index === 1"
+                                    class="grey--text caption"
+                            >(+{{ selectedTickets.length - 1 }} others)</span>
+                        </template>
+                    </v-autocomplete>
+                </v-col>
+            </v-row>
+
+            <v-row>
+                <v-slide-y-transition>
+                    <v-col
+                            v-if="productInfo.length > 0"
+                    >
+                        <v-data-table
+                                :headers="headers"
+                                :items="productInfo"
+                                class="elevation-1"
+                                show-select
+                                item-key="product_id"
+                        >
+                            <template v-slot:top>
+                                <v-toolbar flat>
+                                    <v-toolbar-title>{{ selectedProject }} Product(s)</v-toolbar-title>
+                                    <v-col cols="12" sm="3">
+                                        <v-btn text icon color="green" @click="getProductInfo()">
+                                            <v-icon>mdi-cached</v-icon>
+                                        </v-btn>
+                                    </v-col>
+                                    <v-spacer></v-spacer>
+                                </v-toolbar>
+                            </template>
+                            <template v-slot:item.action="{ item }">
+                                <v-icon
+                                        small
+                                        class="mr-2"
+                                        @click="editItem(item)"
+                                >
+                                    mdi-account-edit
+                                </v-icon>
+                            </template>
+                            <template v-slot:item.product_type="{ item }">
+                                <v-chip dark color="green">{{ item.product_type }}
+                                </v-chip>
+                            </template>
+                            <template v-slot:item.product_hunt_type="{ item }">
+                                <v-chip dark :color="getHuntTypeColor(item.product_hunt_type)">{{ item.product_hunt_type }}
+                                </v-chip>
+                            </template>
+                        </v-data-table>
+                </v-slide-y-transition>
+                </v-col>
+            </v-row>
+        </v-content>
         <v-bottom-navigation
                 color="success"
         >
@@ -86,12 +179,84 @@ try {
         vuetify: new Vuetify(),
         data: {
             darkThemeSelected: false,
+            dialog: false,
+            projectArray: [],
+            ticketArray: [],
+            selectedProject: '',
+            selectedTickets: [],
+            productInfo: [],
+            headers: [
+                {text: 'Creation DateTime', value: 'product_creation_time'},
+                {text: 'Ticket ID', value: 'ticket_id'},
+                {text: 'Product Type', value: 'product_type'},
+                {text: 'Product Name', value: 'product_name'},
+                {text: 'Product Alt Name', value: 'product_alt_design_name'},
+                {text: 'Product QA Status', value: 'product_qa_status'},
+                {text: 'Hunt Type', value: 'product_hunt_type'},
+                {text: 'Actions', value: 'action', sortable: false, align: 'center'},
+            ],
+        },
+        methods: {
+            getHuntTypeColor(hunt_type) {
+                let color = '';
+                if (hunt_type === 'probe') {
+                    color = 'purple';
+                } else if (hunt_type === 'radar') {
+                    color = 'orange';
+                } else {
+                    color =  'blue';
+                }
+                return color;
+            },
+            getProjectList() {
+                this.overlay = true;
+                axios.get('api/fetch_project_list.php')
+                    .then((response) => {
+                        this.projectArray = response.data[0].project_info;
+                        this.overlay = false;
+                    });
+            },
+            getTicketList() {
+                this.overlay = true;
+                let formData = new FormData;
+                formData.append('db_name', this.selectedProject);
+                axios.post('api/fetch_project_ticket_list.php', formData)
+                    .then((response) => {
+                        this.selectedTickets = []
+                        this.ticketArray = response.data[0].ticket_info;
+                        this.overlay = false;
+                    });
+
+            },
+            getProductInfo() {
+                if (this.selectedProject !== '' && this.selectedTickets.length !== 0) {
+                    this.overlay = true;
+                    let formData = new FormData;
+                    formData.append('db_name', this.selectedProject);
+                    formData.append('ticket', this.selectedTickets);
+                    axios.post('api/fetch_product_info.php', formData)
+                        .then((response) => {
+                            this.productInfo = [];
+                            this.productInfo = response.data[0].product_info;
+                            this.overlay = false;
+                        });
+                }
+            }
         },
         watch: {
             darkThemeSelected: function (val) {
                 this.$vuetify.theme.dark = val;
             },
+            selectedProject: function () {
+                this.getTicketList();
+            },
+            selectedTickets: function() {
+                this.getProductInfo();
+            },
         },
+        created() {
+            this.getProjectList();
+        }
     });
 </script>
 <style>
