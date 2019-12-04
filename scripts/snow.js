c15b0af8afe198ad211838acd25a5e52ea1226a8
Dancer-14863 <g14863.malika@gmail.com>
2019-12-04 18:52:43 +0530

    Added add feature

--- scripts/snow.js
+++ scripts/snow.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
             if (data === "false") {
                 $(document).snowfall({image: "scripts/flake.png", minSize: 10, maxSize: 15, maxSpeed: 2, flakeCount: 10});
             } else {
-                $(document).snowfall({image: "scripts/heshan.png", minSize: 45, maxSize: 60, maxSpeed: 2, flakeCount: 10});
+                $(document).snowfall({image: "scripts/heshan.png", minSize: 45, maxSize: 60, maxSpeed: 2, flakeCount: 20});
             }
             $(document).snowfall();
         });
