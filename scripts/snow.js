b034179d20d6302c00b992b26d58e24d3c808f6e
Dancer-14863 <g14863.malika@gmail.com>
2019-12-09 18:21:19 +0530

    Patch

--- scripts/snow.js
+++ scripts/snow.js
@@ -4,11 +4,7 @@ $(document).ready(function(){
     if (n === 11) {
         //Start the snow default options you can also make it snow in certain elements, etc.
         $.get('verify.php', function(data) {
-            if (data === "false") {
-                $(document).snowfall({image: "scripts/flake.png", minSize: 10, maxSize: 15, maxSpeed: 2, flakeCount: 10});
-            } else {
-                $(document).snowfall({image: "scripts/heshan.png", minSize: 45, maxSize: 60, maxSpeed: 2, flakeCount: 20});
-            }
+            $(document).snowfall({image: "scripts/flake.png", minSize: 10, maxSize: 15, maxSpeed: 2, flakeCount: 10});
             $(document).snowfall();
         });
         //$(document).snowfall('clear');
