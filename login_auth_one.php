405df2fc07f319ef651a7965a5e3d860c0a0f27c
randikagss <g14830.randika@gmail.com>
2020-02-05 13:59:54 +0530

    add new login page

--- login_auth_one.php
+++ login_auth_one.php
@@ -103,9 +103,9 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
   <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" type="text/css" href="styles/loginnow.css" />
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel='icon' href='favicon.ico' type='image/x-icon' />
+    <link rel="stylesheet" type="text/css" href="styles/login1.css" />
     <link rel="stylesheet" type="text/css" href="styles/main.css" />
     <link rel="stylesheet" type="text/css" href="styles/login.css"/>
     <script src="scripts/transition.js"></script>
